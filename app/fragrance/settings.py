608af478c654e0303463d0334d4c38a0fa273cb6
Edgaras Mickus <edgaras.mickus@gmail.com>
2020-01-28 15:46:54 +0200

    Postgres, gunicorn and nginx added into production Dockerfile

--- app/fragrance/settings.py
+++ app/fragrance/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'fragrance.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", 'django.db.backends.sqlite3'),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, 'db.sqlite3')),
+        "USER": os.environ.get("SQL_USER"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD"),
+        "HOST": os.environ.get("SQL_HOST"),
+        "PORT": os.environ.get("SQL_PORT"),
     }
 }
 
